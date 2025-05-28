c64d1f6e3eca89c41dd4b993bb28bd804c347012
Dilan <dilanderegizu@icloud.com>
2025-05-28 20:53:11 +0300

    Update tarot.router.js

--- Astro_Backend/routers/tarot.router.js
+++ Astro_Backend/routers/tarot.router.js
@@ -1,1 +1,17 @@
 
+const controller = require("../controllers/tarot.controller");
+const router = require("express").Router();
+
+router.post("/createTarot", controller.createTarot);
+
+router.put("/updateTarot", controller.updateTarot);
+
+router.get("/getAllTarot", controller.getAllTarot);
+
+router.get("/getRandomTarot", controller.getRandomTarot);
+
+router.delete("/deleteTarot", controller.deleteTarot);
+
+module.exports = {
+  tarot: router,
+};
