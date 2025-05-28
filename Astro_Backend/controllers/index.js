a617ab3ef29fc2eb4ffee16b402796ec636121e0
Dilan <dilanderegizu@icloud.com>
2025-05-28 20:52:34 +0300

    Update index.js

--- Astro_Backend/controllers/index.js
+++ Astro_Backend/controllers/index.js
@@ -2,10 +2,11 @@ const blog = require("./blog.controller");
 const user = require("./user.controller");
 const zodiac = require("./zodiac.controller");
 const complatibility = require("./compatibility.controller");
-
+const tarot = require("./tarot.controller")
 module.exports = {
   blog,
   user,
   zodiac,
-  complatibility 
+  complatibility ,
+  tarot
 };
