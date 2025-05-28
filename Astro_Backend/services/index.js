c0396bc22e21231c22086734cbf4ab3af0f8e7b7
Dilan <dilanderegizu@icloud.com>
2025-05-28 20:54:06 +0300

    Update index.js

--- Astro_Backend/services/index.js
+++ Astro_Backend/services/index.js
@@ -2,10 +2,12 @@ const blogService = require("./blog.service");
 const userService = require("./user.service");
 const zodiacService = require("./zodiac.service");
 const complatibilityService = require("./compatibility.service");
+const tarotService = require("./tarot.service");
 
 module.exports = {
-  blog:blogService,
-  user:userService,
-  zodiac:zodiacService,
-  compatibility:complatibilityService
+  blog: blogService,
+  user: userService,
+  zodiac: zodiacService,
+  compatibility: complatibilityService,
+  tarot: tarotService,
 };
