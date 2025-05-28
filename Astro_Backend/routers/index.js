a1275390a4e09a5dec426dc8990f2915255c7fa4
Dilan <dilanderegizu@icloud.com>
2025-05-28 20:53:24 +0300

    Update index.js

--- Astro_Backend/routers/index.js
+++ Astro_Backend/routers/index.js
@@ -2,9 +2,11 @@ const zodiacRouter = require("./zodiac.router").zodiac;
 const userRouter = require("./user.router").user;
 const blogRouter = require("./blog.router").blog;
 const compatibilityRouter = require("./compatibility.router").compatibility;
+const tarotRouter = require("./tarot.router").tarot;
 module.exports = {
   zodiacRouter,
   userRouter,
   blogRouter,
   compatibilityRouter,
+  tarotRouter,
 };
